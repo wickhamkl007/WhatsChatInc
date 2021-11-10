7b398761a030ae3c01d95b6982ecb2fa4c350413
Taylor Powell <taylor.powell2001@gmail.com>
2021-11-10 14:43:47 -0700

    Redid chat stuff

--- WhatsChatApp/chat/views.py
+++ WhatsChatApp/chat/views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render
 
-def chat_room(request, label):
-    room, created = Room.objects.get_or_create(label=label)
-
-    messages = reversed(room.messages.order_by('-timestamp')[:50])
-
-    return render(request, "chat/room.html",{
-        'room': room,
-        'messages': messages,
-        })
+def index(request):
+    return render(request, 'chat/index.html')
--- WhatsChatApp/config/settings.py
+++ WhatsChatApp/config/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages',
+    'chat',
     'channels',
 ]
 
@@ -123,13 +126,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "asgi_redis.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
-        },
-        "ROUTING": "chat.routing.channel_routing",
-    },
-}
+ASGI_APPLICATION = "WhatsChatApp.asgi.application"
