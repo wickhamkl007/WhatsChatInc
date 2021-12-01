4afd17bb612b1bb66d7fb2f84c0fd29f440f3fbd
Taylor Powell <taylor.powell2001@gmail.com>
2021-12-01 14:43:58 -0700

    Changed urls.py

--- WhatsChatApp/config/urls.py
+++ WhatsChatApp/config/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')), 
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('chat/', include('chat.urls')),
     path('accounts/', include('accounts.urls')), 
     path('', include('homepage.urls')),
 ]
