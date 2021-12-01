344a98b18363d9586b01c13090bfec047b5b3189
Taylor Powell <taylor.powell2001@gmail.com>
2021-12-01 14:39:18 -0700

    Changed ASGI.py

--- WhatsChatApp/config/asgi.py
+++ WhatsChatApp/config/asgi.py
@@ -9,8 +9,17 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
-application = get_asgi_application()
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WhatsChatApp.settings')
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+     "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
