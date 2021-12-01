152dc6888366dba73026c8c56c6686ee3748bf2f
Taylor Powell <taylor.powell2001@gmail.com>
2021-12-01 14:13:56 -0700

    Removed heroku stuff

--- WhatsChatApp/config/asgi.py
+++ WhatsChatApp/config/asgi.py
@@ -10,12 +10,16 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter, get_default_application
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WhatsChatApp.settings')
-django.setup()
-application = get_default_application()
-
-
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+     "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
--- WhatsChatApp/config/settings.py
+++ WhatsChatApp/config/settings.py
@@ -132,13 +132,16 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Channels
-ASGI_APPLICATION = 'WhatsChatApp.asgi.application'
+ASGI_APPLICATION = 'config.asgi.application'
 CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {"hosts": [os.environ.get("REDIS_URL", "redis://localhost:6379")]},
-    },
-}
+    'default':{
+        'BACKEND': "channels.layers.InMemoryChannelLayer"
+        #"channels_redis.core.RedisChannelLayer",
+        #'CONFIG': {
+            #"hosts": [('127.0.0.1', 6379)],
+            #},
+        },
+    }
 
 STATIC_URL = 'WhatsChatApp/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
